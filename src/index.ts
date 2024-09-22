46f216a68c1f25ecea85672b0c52f983f921d541
ariel <arieldelgrande@hotmail.com>
2024-09-21 21:38:10 -0700

    adding a response

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,22 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  console.log("ariel");
+  res.json({
+    ok: true,
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
+  console.log("hola");
 });
 
 console.log("hola mundo ariel");
