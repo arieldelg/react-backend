0ea62eff18beb95a46830fd10af9120d65f9d2a1
arieldelg <arieldelgrande@hotmail.com>
2024-09-29 20:08:17 -0700

    chanfing url

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use("/api/calendarEvents", routerEvent);
 app.use("*", (_, res) => {
   const __dirname = path.resolve();
 
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "src/public/index.html"));
 });
 
 //listening Port
